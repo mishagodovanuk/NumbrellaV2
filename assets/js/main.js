369454714c94fe123409d3db399daab479df9786
mishagodovanuk <mishagodovanuk@gmail.com>
2024-01-31 14:37:14 +0200

    add base styles

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+function showPopup() {
+    // Show the popup and overlay
+    document.getElementById('popup').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block';
+}
+
+function hidePopup() {
+    // Hide the popup and overlay
+    document.getElementById('popup').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
+}
