e68b6fc539b0c429672dce687bac71081cf281f5
mishagodovanuk <mishagodovanuk@gmail.com>
2024-01-31 15:07:09 +0200

    add reload button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,24 @@
+/**
+ * Show hint popup.
+ */
 function showPopup() {
     // Show the popup and overlay
     document.getElementById('popup').style.display = 'block';
     document.getElementById('overlay').style.display = 'block';
 }
 
+/**
+ * Hide hint popup.
+ */
 function hidePopup() {
     // Hide the popup and overlay
     document.getElementById('popup').style.display = 'none';
     document.getElementById('overlay').style.display = 'none';
+}
+
+/**
+ * Reload game.
+ */
+function reload() {
+    location.reload();
 }
